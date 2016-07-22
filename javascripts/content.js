ff27aa8c307cf9169f86c975d4549fbb25e1a31c
Zheng  Weihan <zhengweihan.91@gmail.com>
2016-07-22 20:23:19 +0800

    added content.

---
+++ javascripts/content.js
@@ -0,0 +1,11 @@
+/**
+ * Created by weihan on 22/7/16.
+ */
+
+document.getElementById("intro").textContent =
+  ", my name is Weihan. I am currently pursuing a degree in Computer Science and Business in National University of Singapore. " +
+  "I have lived in Singapore all my life, with brief exchange in Sweden and a work placement in Israel. " +
+  "I enjoy reading, playing basketball and travelling, and is always up for an adventure.";
+
+document.getElementById("now").textContent =
+  "Finishing up my internship with Goldman Sachs Singapore and heading back to school for my final year.";
